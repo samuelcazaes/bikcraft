ccb3a5e9a57f282df0c9b815b18a3498e21d21ba
Samuel Cazaes <samuelcazaes@gmail.com>
2022-09-09 13:59:14 -0300

    Glaeria JS

--- js/script.js
+++ js/script.js
@@ -56,3 +56,24 @@ function eventosPerguntas(pergunta) {
 console.log(perguntas);
 
 perguntas.forEach(eventosPerguntas);
+
+//_____________________________________________________________
+
+//Galeria
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) galeriaContainer.prepend(img);
+
+  console.log(img);
+}
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
