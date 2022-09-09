afa122d3e7305623ca93152b57c4f9dce853baf0
Samuel Cazaes <samuelcazaes@gmail.com>
2022-09-09 13:48:29 -0300

    Perguntas Frequentes JS

--- js/script.js
+++ js/script.js
@@ -28,3 +28,31 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+//_____________________________________________________________
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+
+  const ativa = resposta.classList.contains("ativa");
+
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(pergunta);
+}
+
+function eventosPerguntas(pergunta) {
+  console.log(pergunta);
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+console.log(perguntas);
+
+perguntas.forEach(eventosPerguntas);
